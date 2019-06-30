63e70474a9cbdd51fc8ed0e65a5ad474c6f8ef71
tigs001 <sferguson@tigint.com>
2019-06-30 19:31:28 +1000

    First working web api for shopping cart add and contents.

--- config/module.config.php
+++ config/module.config.php
@@ -5,14 +5,54 @@ namespace ShoppingCart;
 return array(
     'router' => array(
         'routes' => array(
+            'shoppingcart' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route'    => '/shoppingcart',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'ShoppingCart\Controller',
+                    	'controller' 	=> 'ShoppingCartController',
+                        'action'     	=> 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'controlleraction' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:action]',
+                            'constraints' => array(
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            	'controller' => 'ShoppingCartController',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
 
         ),
     ),
-    'controller_plugins' => array (
+    'controllers' => array(
+        'factories' => array(
+        	/*
+        	 * We need to use factories now, instead of invokables.  6/10/18
+        	 *
+        	 * We should be able to use class names on the left, but due to
+        	 * historical naming conventions, we do not refer elsewhere in code
+        	 * to the name "IndexController", but just "Index".
+        	 *
+        	 * Hence the left of these controllers must be a string, not a ::class.
+        	 */
+        	'ShoppingCart\Controller\ShoppingCartController' 	=> Factory\ShoppingCartControllerFactory::class,
+        ),
+    ),
+	'controller_plugins' => array (
         'factories' => array (
             'ShoppingCart' => Factory\ShoppingCartPluginFactory::class,
         ),
-    ),
+	),
     'view_helpers' => array (
         'factories' => array (
             'ShoppingCart' => Factory\ShoppingCartViewHelperFactory::class,
--- src/Controller/ShoppingCartController.php
+++ src/Controller/ShoppingCartController.php
@@ -3,6 +3,9 @@
 namespace ShoppingCart\Controller;
 
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
+use Zend\Json\Json;
+
 
 
 /**
@@ -44,6 +47,117 @@ class ShoppingCartController extends \Zend\Mvc\Controller\AbstractActionControll
 
 		return new ViewModel(array());
 	}
+
+
+
+	/**
+	 * indexAction() - A default non action.  Just returns a "result" => "OK".
+	 *
+	 * @return JsonModel - With 'result' as human readable and 'resultcode' integer.  0 = success.
+	 */
+	public function indexAction()
+	{
+		/*
+		 *
+		 */
+
+		return new JsonModel(array(
+							'result' 		=> 'OK',
+							'resultcode' 	=> 0,
+						));
+	}
+
+
+
+	/**
+	 * contentsAction() - Returns the shopping cart contents
+	 *
+	 * @return JsonModel
+	 */
+	public function contentsAction()
+	{
+		/*
+		 *
+		 */
+
+		$cart = $this->ShoppingCart(); 	/* @var $cart \ShoppingCart\Service\ShoppingCart */
+
+		$cartdata = array();
+		foreach ($cart->cart() as $onekey => $oneentity)
+		{
+			/* @var $oneentity \ShoppingCart\Entity\ShoppingCartEntity */
+
+			$onearray = array();
+
+			$reflection = new \ReflectionClass($oneentity);
+			foreach ($reflection->getProperties() as $prop)
+			{
+				/*
+				 * The existing ShoppingCartEntity uses the strategy where
+				 * an underscore is converted to a name without underscores,
+				 * and the following letter is capitalised.
+				 */
+				// $getmethod = 'get' . ucfirst($prop->getName());
+				$getmethod = 'get' . implode('', array_map('ucfirst', explode('_', $prop->getName())));
+				$onearray[$prop->getName()] = $oneentity->$getmethod();
+			}
+
+			$cartdata[$onekey] = $onearray;
+		}
+
+
+		$contents = new JsonModel(array(
+								'contents' 		=> $cartdata,
+								'total_sum' 	=> $cart->total_sum(),
+								'total_items' 	=> $cart->total_items(),
+							));
+
+		return $contents;
+	}
+
+
+
+	/**
+	 * addAction() - Adds itesm to the shopping cart
+	 *
+	 * @return JsonModel With 'result' as human readable and 'resultcode' integer.  0 = success.
+	 */
+	public function addAction()
+	{
+		/*
+		 *
+		 */
+
+		$cart = $this->ShoppingCart(); 	/* @var $cart \ShoppingCart\Service\ShoppingCart */
+
+		/*
+		 * Get the json payload.
+		 */
+		$request = $this->getRequest();
+		if ($request->isXmlHttpRequest())
+		{
+			$payload = $request->getContent();
+			$jdata = json_decode($payload, true);
+		}
+
+
+		/*
+		 * Now add the JSON data into our shopping cart.
+		 */
+		$cart->insert($jdata);
+
+
+		/*
+		 * Return success.
+		 */
+		$contents = new JsonModel(array(
+								'result' 		=> 'OK',
+								'resultcode' 	=> 0,
+							));
+
+		return $contents;
+	}
+
 }
 
 
